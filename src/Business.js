e3701b2a955030df28c34fffeaabb9c834b600f6
JennBlunt <jennbluntphoto@gmail.com>
2019-08-21 12:15:16 -0400

    First GH commit. Next step - hook up Yelp API

---
+++ src/Business.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+
+
+export default class Business extends Component {
+    render() {
+        const { business } = this.props;
+        return (
+                <div className="Business">
+                    <div className="image-container">
+                        <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+                    </div>
+                    <h2>MarginOtto Pizzeria</h2>
+                    <div className="Business-information">
+                        <div className="Business-address">
+                            <p> { business.address } </p>
+                            <p> { business.city } </p>
+                            <p> { business.state} { business.zipCode} </p>
+                        </div>
+
+                        <div className="Business-reviews">
+                            <h3> { business.category } </h3>
+                            <h3 className="rating"> Rating: { business.rating} </h3>
+                            <p> { business.reviewCount } reviews</p>
+                        </div>
+                    </div>
+                </div>
+        )
+    }
+}
