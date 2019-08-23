4352cdfb5dbdbbe30de8244da966b540492280f0
JennBlunt <jennbluntphoto@gmail.com>
2019-08-23 14:06:47 -0400

    yelp api functioning. styling updates

--- src/BusinessList.js
+++ src/BusinessList.js
@@ -4,13 +4,13 @@ import Business from './Business';
 export default class BusinessList extends Component {
     render() {
         return (
-            <div className='BusinessList'>
-               {
-                   this.props.businesses.map((business) => {
-                       return <Business business={ business } />
-                   })
-               }
-            </div>
-        )
+          <div className="BusinessList">
+            {
+              this.props.businesses.map(business => {
+                return <Business business={business} key={business.id} />
+              })
+            }
+          </div>
+        );
+      }
     }
-}
